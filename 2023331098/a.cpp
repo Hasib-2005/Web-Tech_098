00156399d51154e428fdc00229c2c95f9c0093bb
Hasib-2005 <hasib.nextgen@gmail.com>
2026-06-22 09:34:16 +0600

    modified

---
+++ 2023331098/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl "\n"
+#define int long long
+#define yes cout << "YES" << nl;
+#define no cout << "NO" << nl;
+#define vi vector<int>
+#define ip(x) for(auto &it : x) cin >> it
+#define all(x) x.begin(),x.end()
+
+// - Mohammad Hasibur Rahman
+
+void solve(){
+
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+}
